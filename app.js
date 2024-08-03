f8ef48d38d63a90516f4ac18226b6ef7332e4a4c
alan890729 <alan890729@gmail.com>
2024-08-03 18:14:40 +0800

    router init

--- app.js
+++ app.js
@@ -4,7 +4,60 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('on route: /')
+    res.redirect('/todos')
+})
+
+app.get('/todos', (req, res) => {
+    res.send(`
+        <p>GET /todos</p>
+        <p>You can see all todos here.</p>
+    `)
+})
+
+app.get('/todos/new', (req, res) => {
+    res.send(`
+        <p>GET /todos/new</p>
+        <p>You can add new todo here. This is a page for adding todos. Enter some todo details and press 'Create' to create a todo.</p>
+    `)
+})
+
+app.post('/todos', (req, res) => {
+    res.send(`
+        <p>POST /todos</p>
+        <p>Successfully add a todo.</p>    
+    `)
+})
+
+app.get('/todos/:id', (req, res) => {
+    const todoId = req.params.id
+    res.send(`
+        <p>GET /todos/${todoId}</p>
+        <p>A specific todo in todos, this is todo id: ${todoId}</p>    
+    `)
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+    const todoId = req.params.id
+    res.send(`
+        <p>GET /todos/${todoId}/edit</p>
+        <p>This is a page use for edit a todo in todos. You now editting todo id: ${todoId}</p>
+    `)
+})
+
+app.put('/todos/:id', (req, res) => {
+    const todoId = req.params.id
+    res.send(`
+        <p>PUT /todos/${todoId}</p>
+        <p>todo id: ${todoId} has been modified</p>
+    `)
+})
+
+app.delete('/todos/:id', (req, res) => {
+    const todoId = req.params.id
+    res.send(`
+        <p>DELETE /todos/${todoId}</p>   
+        <p>todo id: ${todoId} has been deleted</p> 
+    `)
 })
 
 app.listen(port, () => { 
