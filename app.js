cdebc1b133d98cee02e9339e10b5fa88059282a6
alan890729 <alan890729@gmail.com>
2024-08-10 02:07:45 +0800

    implement updating todo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const db = require('./models')
 const { engine } = require('express-handlebars')
+const methodOverride = require('method-override')
 
 const app = express()
 const port = 3000
@@ -11,6 +12,7 @@ app.set('view engine', '.hbs')
 app.set('views', './views')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -62,18 +64,34 @@ app.get('/todos/:id', (req, res) => {
 
 app.get('/todos/:id/edit', (req, res) => {
     const todoId = req.params.id
-    res.send(`
-        <p>GET /todos/${todoId}/edit</p>
-        <p>This is a page use for edit a todo in todos. You now editting todo id: ${todoId}</p>
-    `)
+
+    return Todo.findByPk(todoId, {
+        attributes: [
+            'id',
+            'name'
+        ],
+        raw: true
+    }).then((todo) => {
+        res.render('edit', { todo })
+    }).catch((err) => {
+        res.status(422).json(err)
+    })
 })
 
 app.put('/todos/:id', (req, res) => {
     const todoId = req.params.id
-    res.send(`
-        <p>PUT /todos/${todoId}</p>
-        <p>todo id: ${todoId} has been modified</p>
-    `)
+    const { name } = req.body
+
+    return Todo.update(
+        { name },
+        {
+            where: { id: Number(todoId) }
+        }
+    ).then(() => {
+        res.redirect(`/todos/${todoId}`)
+    }).catch((err) => {
+        res.status(422).json(err)
+    })
 })
 
 app.delete('/todos/:id', (req, res) => {
