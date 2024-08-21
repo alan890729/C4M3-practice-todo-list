37e74573e71935039173535eed351fb82a68c4b2
alan890729 <alan890729@gmail.com>
2024-08-21 23:28:42 +0800

    add basic structure for user todos

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const router = express.Router()
 const todosRouter = require('./todos')
+const userRouter = require('./users')
 
 router.use('/todos', todosRouter)
+router.use('/users', userRouter)
 
 router.get('/', (req, res) => {
     res.redirect('/todos')
+})
+
+router.get('/login', (req, res) => {
+    return res.render('login')
+})
+
+router.post('/login', (req, res) => {
+    return res.send(req.body)
+})
+
+router.post('/logout', (req, res) => {
+    return res.send('on route: POST /users/logout, user logging out')
+})
+
+router.get('/register', (req, res) => {
+    return res.render('register')
 })
 
 module.exports = router
